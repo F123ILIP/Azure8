b5208328464d37eb7ae55ad1ffb5b06a3ec77c8d
Filip <filip.przyczyna@gmail.com>
2025-12-03 19:16:47 +0100

    Aktualizacja tekstu powitalnego

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
 from flask import Flask
-import os
-import pyodbc
-
+import datetime
 app = Flask(__name__)
-def get_db_connection():
-    conn_str = os.environ.get('CUSTOMCONNSTR_DB_CONNECTION_STR')
-    conn = pyodbc.connect(conn_str)
-    return conn
 
 @app.route('/')
-def index():
-    tasks_html = "<h2>Lista zadan z bazy danych:</h2><ul>"
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT Title FROM Tasks")
-        rows = cursor.fetchall()
-        for row in rows:
-            tasks_html += f"<li>{row.Title}</li>"
-        conn.close()
-    except Exception as e:
-        tasks_html += f"<li>Blad polaczenia z baza: {str(e)}</li>"
+def hello():
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"<h1>Witaj! Aplikacja wdrożona przez GitHub Actions!</h1>
 
-    tasks_html += "</ul>"
-    return tasks_html
+if __name__ == '__main__':
+    app.run()
